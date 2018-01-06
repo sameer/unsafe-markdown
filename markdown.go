525e27b1ab603ce2064fdef30c1de5b8f040286d
Sameer <spuri4096@gmail.com>
2018-01-06 11:24:21 -0500

    Clean up regexes, finish basic tests, handle all possible EOL combos, fix errors in impl of regexps, write br adder

--- markdown.go
+++ markdown.go
@@ -2,24 +2,59 @@ package markdown
 
 import (
 	"regexp"
-
 )
 
+const (
+	allEOLChars = "\\r\\n\\v\\f\\p{Zl}\\p{Zp}"
+)
 var (
+	// All constructs are restricted to operating on a single line
+
+	// EOLs
+	br = regexp.MustCompile("(?:\\r\\n|\\n\\r|\\n|\\r|\\p{Zl}|\\p{Zp}|\\v|\\f)")
 	// BOL, 1 to 6 #s, a space, some text, EOL.
-	header  = regexp.MustCompile("(?m)^([#]{1,6})[ ]([\\s\\S]+?)$")
-	// 1 star, char that's not a star, optional text, star, char that's not a star or is EOL. Multiline.
-	italics = regexp.MustCompile("[*]{1}([^*][\\s\\S]*?[*]{1}[^*$]{1})")
-	// 2 stars, char that's not a star, optional text, 2 stars, char that's not a star. Multiline.
-	bold = regexp.MustCompile("[*]{2}([^*][\\s\\S]*?[*]{1}[^*]{2})")
-	// >, a space, and required text.
-	blockquote = regexp.MustCompile("(?m)>[ ]([\\s\\S]+?)$")
-	// non ! char, [, required text, ], (, optional text, ).
-	link = regexp.MustCompile("[^!]\\[([\\s\\S])+?\\]\\(([\\s\\S]*?)\\)")
-	// !, [, required text, ], (, optional text, ).
-	img = regexp.MustCompile("!\\[([\\s\\S])+?\\]\\(([\\s\\S]*?)\\)")
+	header = regexp.MustCompile("(?m)^([#]{1,6})[ ]([^"+allEOLChars+"]+?)$")
+	// BOL, optional text, 1 star, optional char that's not a star, required text, 1 star, optional char that's not a star, optional text, EOL. Requires >= 2 characters inside.
+	italics = regexp.MustCompile("(?m)^[^"+allEOLChars+"*]*?[*]([^*"+allEOLChars+"][^"+allEOLChars+"]*[^*"+allEOLChars+"])[*][^"+allEOLChars+"*]*?$")
+	// Duplicate of italics except with two stars.
+	bold = regexp.MustCompile("(?m)^[^"+allEOLChars+"*]*?[*]{2}([^*"+allEOLChars+"][^"+allEOLChars+"]*[^*"+allEOLChars+"])[*]{2}[^"+allEOLChars+"*]*?$")
+	// BOL, >, a space, optional text, EOL.
+	blockquote = regexp.MustCompile("(?m)^>[ ]([^"+allEOLChars+"]*?)$")
+	// Plug this into an online regexp explainer and you'll see why
+	link = regexp.MustCompile(`(?m)(?:^|^[^`+allEOLChars+`]*?[^!])\[([^`+allEOLChars+`]+?)\]\(([^`+allEOLChars+`]*?)\)[^`+allEOLChars+`]*?$`)
+	// Plug this into an online regexp explainer and you'll see why
+	img = regexp.MustCompile(`(?m)^[^`+allEOLChars+`]*?!\[([^`+allEOLChars+`]+?)\]\(([^`+allEOLChars+`]*?)\)[^`+allEOLChars+`]*?$`)
 )
 
 func MarkdownToHtml(md string) string {
-	return ""
+	html := md
+	matched := 0
+	for {
+
+		if matched == 0 {
+			break
+		}
+	}
+	html = replaceBr(html)
+	return html
+}
+
+func replaceBr(md string) string {
+	brMatches := br.FindAllStringIndex(md, -1)
+	reverse(brMatches)
+	for _, match := range brMatches {
+		temp := md[:match[0]] + "<br>" + md[match[0]:match[1]]
+		if len(md) > match[1] {
+			temp += md[match[1]:]
+		}
+		md = temp
+	}
+	return md
+}
+
+func reverse(a [][]int) {
+	for i := len(a)/2 - 1; i >= 0; i-- {
+		opp := len(a) - 1 - i
+		a[i], a[opp] = a[opp], a[i]
+	}
 }
--- markdown_test.go
+++ markdown_test.go
@@ -52,7 +52,105 @@ func TestHeader(t *testing.T) {
 	}
 }
 
+func TestItalics(t *testing.T) {
+	testStrs := []testString{
+		{"*italics*", true},
+		{"bblahblahblahblah*italics      yo!*blah", true},
+		{"** italics **", false},
+		{"\n* **italics** *\n", true},
+		{"\r\n*italics*\n\r", true},
+		{"\r\n*italics\n*\r", false},
+		{"\r\n** *italics* **\n\r", false},
+	}
+	for _, testStr := range testStrs {
+		if val := italics.MatchString(testStr.string); val != testStr.expected {
+			t.Error("For", testStr.string, "expected", fmt.Sprintf("%q", testStr.expected), "got", fmt.Sprintf("%q", val))
+			break
+		}
+	}
+	//
+	//testStrs = []testString{
+	//	{"# h1", [][]string{{"# h1", "#", "h1"}}},
+	//	{"## h2", [][]string{{"## h2", "##", "h2"}}},
+	//	{"### h3", [][]string{{"### h3", "###", "h3"}}},
+	//	{"#### h4", [][]string{{"#### h4", "####", "h4"}}},
+	//	{"##### h5", [][]string{{"##### h5", "#####", "h5"}}},
+	//	{"###### h6", [][]string{{"###### h6", "######", "h6"}}},
+	//	{"## h1 ##", [][]string{{"## h1 ##", "##", "h1 ##"}}},
+	//	{" ## h2", [][]string{}},
+	//	{"", [][]string{}},
+	//	{"\n# h1\n", [][]string{{"# h1", "#", "h1"}}},
+	//	{"\r\n# h1\n\r", [][]string{{"# h1", "#", "h1"}}},
+	//}
+	//for _, testStr := range testStrs {
+	//	if val := italics.FindAllStringSubmatch(testStr.string, -1); !strSliceEqual(val, testStr.expected.([][]string)) {
+	//		t.Error("For", testStr.string, "expected", fmt.Sprintf("%q", testStr.expected), "got", fmt.Sprintf("%q", val))
+	//		break
+	//	}
+	//}
+}
+
+func TestBlockquote(t *testing.T) {
+	testStrs := []testString{
+		{"> woot", true},
+		{">", false},
+		{"> ", true},
+		{"blah blah > sdfsdf", false},
+	}
+	for _, testStr := range testStrs {
+		if val := blockquote.MatchString(testStr.string); val != testStr.expected {
+			t.Error("For", testStr.string, "expected", fmt.Sprintf("%q", testStr.expected), "got", fmt.Sprintf("%q", val))
+			break
+		}
+	}
+}
+
+func TestLink(t *testing.T) {
+	testStrs := []testString{
+		{"It's crazy! [This](link) gives you magical powers!", true},
+		{"It's crazy! ![This](image) gives you magical powers!", false},
+		{"![This](image) shows you one easy trick to make money fast!", false},
+		{"![This](image) shows you one easy trick to make money fast! [This](link) does too!", true},
+		{"![This](image) shows you one easy trick to make money fast! [Th!s](l!nk) does too!", true},
+	}
+	for _, testStr := range testStrs {
+		if val := link.MatchString(testStr.string); val != testStr.expected {
+			t.Error("For", fmt.Sprintf("%q",testStr.string), "expected", fmt.Sprintf("%v", testStr.expected), "got", fmt.Sprintf("%v", val))
+			break
+		}
+	}
+}
+
+func TestImage(t *testing.T) {
+	testStrs := []testString{
+		{"It's crazy! [This](link) gives you magical powers!", false},
+		{"It's crazy! ![This](image) gives you magical powers!", true},
+		{"![This](image) shows you one easy trick to make money fast!", true},
+		{"[This](link) shows you one easy trick to make money fast! ![This](image) does too!", true},
+		{"[This](link) shows you one easy trick to make money fast! ![Th!s](l!nk) does too!", true},
+		{"[This](link) shows you one easy trick to make money fast!![Th!s](l!nk) does too!", true},
+	}
+	for _, testStr := range testStrs {
+		if val := img.MatchString(testStr.string); val != testStr.expected {
+			t.Error("For", fmt.Sprintf("%q",testStr.string), "expected", fmt.Sprintf("%v", testStr.expected), "got", fmt.Sprintf("%v", val))
+			break
+		}
+	}
+}
+
+func TestMarkdownToHtml(t *testing.T) {
+	fmt.Printf("Got %q\n", MarkdownToHtml("line1\nline2\n"))
+}
+
 func strSliceEqual(a, b [][]string) bool {
+	if a == nil && b == nil {
+		return true
+	} else if a == nil || b == nil {
+		if len(a) == len(b) { // nil slice has len 0
+			return true
+		}
+		return false
+	}
 	if len(a) != len(b) {
 		return false
 	}
